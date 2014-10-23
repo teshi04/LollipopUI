e771a64f8749f920e326246d3c6d3f6f8b4aa1ed
teshi <teshi04@gmail.com>
2014-10-23 19:54:53 +0900

    Material Design

--- app/src/main/java/jp/tsur/lollipopui/MainActivity.java
+++ app/src/main/java/jp/tsur/lollipopui/MainActivity.java
@@ -1,20 +1,23 @@
 package jp.tsur.lollipopui;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Spinner;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
     }
 
 
