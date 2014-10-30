00a6f33769afe8dda44824c869c798521d0f43c4
teshi <teshi04@gmail.com>
2014-10-30 13:39:41 +0900

    add TransitionAnimation

---
+++ app/src/main/java/jp/tsur/lollipopui/ImageActivity.java
@@ -0,0 +1,77 @@
+package jp.tsur.lollipopui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class ImageActivity extends ActionBarActivity {
+
+    public static final String EXTRA_IMAGE = "image";
+
+    public static void launch(Activity activity, View transitionView, String url) {
+        ActivityOptionsCompat options =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        activity, transitionView, EXTRA_IMAGE);
+        Intent intent = new Intent(activity, ImageActivity.class);
+        intent.putExtra(EXTRA_IMAGE, url);
+        ActivityCompat.startActivity(activity, intent, options.toBundle());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ActionBar supportActionBar = getSupportActionBar();
+
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeButtonEnabled(true);
+        }
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        ViewCompat.setTransitionName(image, EXTRA_IMAGE);
+        Picasso.with(this).load(getIntent().getStringExtra(EXTRA_IMAGE)).into(image);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image_actvity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    finishAfterTransition();
+                } else {
+                    finish();
+                }
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- app/src/main/java/jp/tsur/lollipopui/MainActivity.java
+++ app/src/main/java/jp/tsur/lollipopui/MainActivity.java
@@ -1,7 +1,9 @@
 package jp.tsur.lollipopui;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -10,9 +12,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String[] MENU = {"normal", "ActivityOptions.makeSceneTransitionAnimation"};
+    private static final String IMAGE_URL = "http://tsur.jp/img/nekouo.png";
 
     private ActionBarDrawerToggle mDrawerToggle;
 
@@ -24,15 +35,53 @@ public class MainActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
         final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         drawerLayout.setDrawerListener(mDrawerToggle);
+        ListView drawerList = (ListView) findViewById(R.id.drawer);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        final ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, MENU);
+        drawerList.setAdapter(adapter);
+
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                drawerLayout.closeDrawers();
+                Intent intent;
+                switch (position) {
+                    case 0:
+                        intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                        // Check if we're running on Android 5.0 or higher
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            // Call some material design APIs here
+                            startActivity(intent,
+                                    ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+                        } else {
+                            // Implement this feature without material design
+                            startActivity(intent);
+                        }
+                }
+            }
+        });
+
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        Picasso.with(this).load(IMAGE_URL).into(imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageActivity.launch(MainActivity.this, findViewById(R.id.image_view), IMAGE_URL);
+            }
+        });
 
         findViewById(R.id.recycler_view_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,7 +91,6 @@ public class MainActivity extends ActionBarActivity {
             }
         });
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
